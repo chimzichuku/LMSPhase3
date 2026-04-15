d4f3100540000e149154b19403dc6feb26a53c26
chimzichuku <chimzi.chuku01@gmail.com>
2026-04-15 11:06:22 -0600

    Initial commit: Started the project setup and then completed methods in Register.cshtml.cs, AdministratorController.cs, and CommonController.cs

---
+++ Controllers/CommonController.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LMS.Models.LMSModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+[assembly: InternalsVisibleTo( "LMSControllerTests" )]
+namespace LMS.Controllers
+{
+    public class CommonController : Controller
+    {
+        private readonly LMSContext db;
+
+        public CommonController(LMSContext _db)
+        {
+            db = _db;
+        }
+
+        /*******Begin code to modify********/
+
+        /// <summary>
+        /// Retreive a JSON array of all departments from the database.
+        /// Each object in the array should have a field called "name" and "subject",
+        /// where "name" is the department name and "subject" is the subject abbreviation.
+        /// </summary>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetDepartments()
+        {
+            var depts = db.Departments.Select(d => new { d.Name, d.Subject });
+            return Json(depts.ToList());
+        }
+
+
+
+        /// <summary>
+        /// Returns a JSON array representing the course catalog.
+        /// Each object in the array should have the following fields:
+        /// "subject": The subject abbreviation, (e.g. "CS")
+        /// "dname": The department name, as in "Computer Science"
+        /// "courses": An array of JSON objects representing the courses in the department.
+        ///            Each field in this inner-array should have the following fields:
+        ///            "number": The course number (e.g. 5530)
+        ///            "cname": The course name (e.g. "Database Systems")
+        /// </summary>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetCatalog()
+        {
+            var catalog = db.Departments
+                .Select(d => new
+                {
+                    subject = d.Subject,
+                    dName = d.Name,
+                    courses = db.Courses
+                        .Where(c => c.Subject == d.Subject)
+                        .Select(c => new { c.Number, c.Name }).ToList()
+                });
+            return Json(catalog.ToList());
+        }
+
+        /// <summary>
+        /// Returns a JSON array of all class offerings of a specific course.
+        /// Each object in the array should have the following fields:
+        /// "season": the season part of the semester, such as "Fall"
+        /// "year": the year part of the semester
+        /// "location": the location of the class
+        /// "start": the start time in format "hh:mm:ss"
+        /// "end": the end time in format "hh:mm:ss"
+        /// "fname": the first name of the professor
+        /// "lname": the last name of the professor
+        /// </summary>
+        /// <param name="subject">The subject abbreviation, as in "CS"</param>
+        /// <param name="number">The course number, as in 5530</param>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetClassOfferings(string subject, int number)
+        {
+            var courseId = db.Courses.FirstOrDefault(c => c.Subject == subject && c.Number == number);
+
+            if (courseId == null)
+            {
+                return Json(new object[] { });
+            }
+
+            var query = db.Classes
+                .Where(c => c.CourseId == courseId.CourseId)
+                .Join(db.Professors,
+                    c => c.ProfessorId,
+                    p => p.UId,
+                    (c,p) => new
+                    {
+                        season = c.Season,
+                        year = c.Year,
+                        location = c.Location,
+                        start = c.Start,
+                        end = c.End,
+                        fname = p.FName,
+                        lname = p.LName
+                    });
+            return Json(query.ToList());
+        }
+
+        /// <summary>
+        /// This method does NOT return JSON. It returns plain text (containing html).
+        /// Use "return Content(...)" to return plain text.
+        /// Returns the contents of an assignment.
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="category">The name of the assignment category in the class</param>
+        /// <param name="asgname">The name of the assignment in the category</param>
+        /// <returns>The assignment contents</returns>
+        public IActionResult GetAssignmentContents(string subject, int num, string season, int year, string category, string asgname)
+        {
+
+            var result = db.Assignments
+                .Where(a => a.Name == asgname)
+                .Join(
+                    db.AssignmentCategories,
+                    a => a.CategoryId,
+                    ac => ac.CategoryId,
+                    (a, ac) => new { a, ac })
+                .Where(cat => cat.ac.Name == category)
+                .Join(
+                    db.Classes,
+                    temp => temp.ac.ClassId,
+                    cl => cl.ClassId,
+                    (temp, cl) => new { temp.a, temp.ac, cl }
+                    )
+                .Where(tempc => tempc.cl.Season == season && tempc.cl.Year == year)
+                .Join(
+                    db.Courses,
+                    temp => temp.cl.CourseId,
+                    co => co.CourseId,
+                    (tempCo, co) => new {tempCo.a, tempCo.ac, tempCo.cl, co}
+                    )
+                .Where(tempCo => tempCo.co.Subject == subject && tempCo.co.Number == num)
+                .Select(cont => cont.a.Content )
+                .FirstOrDefault();
+
+            return Content(result ?? "");
+        }
+
+
+        /// <summary>
+        /// This method does NOT return JSON. It returns plain text (containing html).
+        /// Use "return Content(...)" to return plain text.
+        /// Returns the contents of an assignment submission.
+        /// Returns the empty string ("") if there is no submission.
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="category">The name of the assignment category in the class</param>
+        /// <param name="asgname">The name of the assignment in the category</param>
+        /// <param name="uid">The uid of the student who submitted it</param>
+        /// <returns>The submission text</returns>
+        public IActionResult GetSubmissionText(string subject, int num, string season, int year, string category, string asgname, string uid)
+        {            
+            
+            var result = db.Assignments
+                .Where(a => a.Name == asgname)
+                .Join(
+                    db.AssignmentCategories,
+                    a => a.CategoryId,
+                    ac => ac.CategoryId,
+                    (a, ac) => new
+                        { a, ac })
+                .Where(cat => cat.ac.Name == category)
+                .Join(
+                    db.Classes,
+                    temp => temp.ac.ClassId,
+                    cl => cl.ClassId,
+                    (temp, cl) => new { temp.a, temp.ac, cl }
+                )
+                .Where(tempc => tempc.cl.Season == season && tempc.cl.Year == year)
+                .Join(
+                    db.Courses,
+                    temp => temp.cl.CourseId,
+                    co => co.CourseId,
+                    (tempCo, co) => new {tempCo.a, tempCo.ac, tempCo.cl, co}
+                )
+                .Where(tempCo => tempCo.co.Subject == subject && tempCo.co.Number == num)
+                .Join(
+                    db.Submissions,
+                    temp => temp.a.AssignmentId,
+                    s => s.AssignmentId,
+                    (temp, s) => new {temp.a, temp.ac, temp.cl, temp.co, s}
+                    )
+                .Where(tempS => tempS.s.StudentId == uid)
+                .Select(cont => cont.s.Contents )
+                .FirstOrDefault();
+            
+            return Content(result ?? "");
+        }
+
+
+        /// <summary>
+        /// Gets information about a user as a single JSON object.
+        /// The object should have the following fields:
+        /// "fname": the user's first name
+        /// "lname": the user's last name
+        /// "uid": the user's uid
+        /// "department": (professors and students only) the name (such as "Computer Science") of the department for the user. 
+        ///               If the user is a Professor, this is the department they work in.
+        ///               If the user is a Student, this is the department they major in.    
+        ///               If the user is an Administrator, this field is not present in the returned JSON
+        /// </summary>
+        /// <param name="uid">The ID of the user</param>
+        /// <returns>
+        /// The user JSON object 
+        /// or an object containing {success: false} if the user doesn't exist
+        /// </returns>
+        public IActionResult GetUser(string uid)
+        {
+
+            var admin = 
+                db.Administrators
+                .Where(a => a.UId == uid)
+                .Select(a => new
+                {
+                    fname = a.FName, 
+                    lname = a.LName, 
+                    uid = a.UId
+                })
+                .FirstOrDefault();
+
+            if (admin != null)
+            {
+                return Json(admin);
+            }
+            
+            var prof = 
+                db.Professors
+                    .Where(p => p.UId == uid)
+                    .Join(
+                        db.Departments,
+                        p => p.Subject,
+                        d => d.Subject,
+                        (p, d) => new
+                        {
+                            fname = p.FName,
+                            lname = p.LName,
+                            uid = p.UId,
+                            department = d.Name
+                        }
+                        )
+                    .FirstOrDefault();
+            
+            if (prof != null)
+            {
+                return Json(prof);
+            }
+            
+            var stud = 
+                db.Students
+                    .Where(s => s.UId == uid)
+                    .Join(
+                        db.Departments,
+                        s => s.Major,
+                        d => d.Subject,
+                        (s, d) => new
+                        {
+                            fname = s.FName,
+                            lname = s.LName,
+                            uid = s.UId,
+                            department = d.Name
+                        }
+                    )
+                    .FirstOrDefault();
+            
+            if (stud != null)
+            {
+                return Json(stud);
+            }
+            
+            return Json(new { success = false });
+        }
+
+
+        /*******End code to modify********/
+    }
+}
+
---
+++ Models/LMSModels/LMSContext.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LMS.Models.LMSModels
+{
+    public partial class LMSContext : DbContext
+    {
+        public LMSContext()
+        {
+        }
+
+        public LMSContext(DbContextOptions<LMSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Administrator> Administrators { get; set; } = null!;
+        public virtual DbSet<Assignment> Assignments { get; set; } = null!;
+        public virtual DbSet<AssignmentCategory> AssignmentCategories { get; set; } = null!;
+        public virtual DbSet<Class> Classes { get; set; } = null!;
+        public virtual DbSet<Course> Courses { get; set; } = null!;
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+        public virtual DbSet<Enrolled> Enrolleds { get; set; } = null!;
+        public virtual DbSet<Professor> Professors { get; set; } = null!;
+        public virtual DbSet<Sshkey> Sshkeys { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+        public virtual DbSet<Submission> Submissions { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("name=LMS:LMSConnectionString", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.11.16-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("latin1_swedish_ci")
+                .HasCharSet("latin1");
+
+            modelBuilder.Entity<Administrator>(entity =>
+            {
+                entity.HasKey(e => e.UId)
+                    .HasName("PRIMARY");
+
+                entity.Property(e => e.UId)
+                    .HasMaxLength(8)
+                    .HasColumnName("uID");
+
+                entity.Property(e => e.Dob).HasColumnName("DOB");
+
+                entity.Property(e => e.FName)
+                    .HasMaxLength(100)
+                    .HasColumnName("fName");
+
+                entity.Property(e => e.LName)
+                    .HasMaxLength(100)
+                    .HasColumnName("lName");
+            });
+
+            modelBuilder.Entity<Assignment>(entity =>
+            {
+                entity.HasIndex(e => new { e.Name, e.CategoryId }, "Name")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.CategoryId, "cID_ref_categorys");
+
+                entity.Property(e => e.AssignmentId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("assignmentID");
+
+                entity.Property(e => e.CategoryId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("categoryID");
+
+                entity.Property(e => e.Content).HasMaxLength(8192);
+
+                entity.Property(e => e.Due).HasColumnType("datetime");
+
+                entity.Property(e => e.MaxPoints).HasColumnType("int(10) unsigned");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Assignments)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("cID_ref_categorys");
+            });
+
+            modelBuilder.Entity<AssignmentCategory>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId)
+                    .HasName("PRIMARY");
+
+                entity.HasIndex(e => e.ClassId, "AC_cID_ref_classes");
+
+                entity.HasIndex(e => new { e.Name, e.ClassId }, "Name")
+                    .IsUnique();
+
+                entity.Property(e => e.CategoryId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("categoryID");
+
+                entity.Property(e => e.ClassId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("classID");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.Property(e => e.Weight).HasColumnType("int(10) unsigned");
+
+                entity.HasOne(d => d.Class)
+                    .WithMany(p => p.AssignmentCategories)
+                    .HasForeignKey(d => d.ClassId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("AC_cID_ref_classes");
+            });
+
+            modelBuilder.Entity<Class>(entity =>
+            {
+                entity.HasIndex(e => new { e.CourseId, e.Year, e.Season }, "courseID")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.ProfessorId, "pID_ref_professors");
+
+                entity.Property(e => e.ClassId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("classID");
+
+                entity.Property(e => e.CourseId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("courseID");
+
+                entity.Property(e => e.End).HasColumnType("time");
+
+                entity.Property(e => e.Location).HasMaxLength(100);
+
+                entity.Property(e => e.ProfessorId)
+                    .HasMaxLength(8)
+                    .HasColumnName("professorID");
+
+                entity.Property(e => e.Season).HasMaxLength(6);
+
+                entity.Property(e => e.Start).HasColumnType("time");
+
+                entity.Property(e => e.Year).HasColumnType("int(10) unsigned");
+
+                entity.HasOne(d => d.Course)
+                    .WithMany(p => p.Classes)
+                    .HasForeignKey(d => d.CourseId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("courseID_ref_courses");
+
+                entity.HasOne(d => d.Professor)
+                    .WithMany(p => p.Classes)
+                    .HasForeignKey(d => d.ProfessorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("pID_ref_professors");
+            });
+
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.HasIndex(e => new { e.Number, e.Subject }, "Number")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Subject, "subject_ref_departments");
+
+                entity.Property(e => e.CourseId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("courseID");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.Property(e => e.Number).HasColumnType("int(10) unsigned");
+
+                entity.Property(e => e.Subject).HasMaxLength(4);
+
+                entity.HasOne(d => d.SubjectNavigation)
+                    .WithMany(p => p.Courses)
+                    .HasForeignKey(d => d.Subject)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("subject_ref_departments");
+            });
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasKey(e => e.Subject)
+                    .HasName("PRIMARY");
+
+                entity.Property(e => e.Subject).HasMaxLength(4);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Enrolled>(entity =>
+            {
+                entity.HasKey(e => new { e.StudentId, e.ClassId })
+                    .HasName("PRIMARY")
+                    .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+
+                entity.ToTable("Enrolled");
+
+                entity.HasIndex(e => e.ClassId, "classID_ref_classes");
+
+                entity.Property(e => e.StudentId)
+                    .HasMaxLength(8)
+                    .HasColumnName("studentID");
+
+                entity.Property(e => e.ClassId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("classID");
+
+                entity.Property(e => e.Grade).HasMaxLength(2);
+
+                entity.HasOne(d => d.Class)
+                    .WithMany(p => p.Enrolleds)
+                    .HasForeignKey(d => d.ClassId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("classID_ref_classes");
+
+                entity.HasOne(d => d.Student)
+                    .WithMany(p => p.Enrolleds)
+                    .HasForeignKey(d => d.StudentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("sID_ref_students");
+            });
+
+            modelBuilder.Entity<Professor>(entity =>
+            {
+                entity.HasKey(e => e.UId)
+                    .HasName("PRIMARY");
+
+                entity.HasIndex(e => e.Subject, "subject_ref_department");
+
+                entity.Property(e => e.UId)
+                    .HasMaxLength(8)
+                    .HasColumnName("uID");
+
+                entity.Property(e => e.Dob).HasColumnName("DOB");
+
+                entity.Property(e => e.FName)
+                    .HasMaxLength(100)
+                    .HasColumnName("fName");
+
+                entity.Property(e => e.LName)
+                    .HasMaxLength(100)
+                    .HasColumnName("lName");
+
+                entity.Property(e => e.Subject).HasMaxLength(4);
+
+                entity.HasOne(d => d.SubjectNavigation)
+                    .WithMany(p => p.Professors)
+                    .HasForeignKey(d => d.Subject)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("subject_ref_department");
+            });
+
+            modelBuilder.Entity<Sshkey>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("sshkey");
+
+                entity.Property(e => e.Sshkey1)
+                    .HasColumnType("text")
+                    .HasColumnName("sshkey");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.UId)
+                    .HasName("PRIMARY");
+
+                entity.HasIndex(e => e.Major, "major_ref_department");
+
+                entity.Property(e => e.UId)
+                    .HasMaxLength(8)
+                    .HasColumnName("uID");
+
+                entity.Property(e => e.Dob).HasColumnName("DOB");
+
+                entity.Property(e => e.FName)
+                    .HasMaxLength(100)
+                    .HasColumnName("fName");
+
+                entity.Property(e => e.LName)
+                    .HasMaxLength(100)
+                    .HasColumnName("lName");
+
+                entity.Property(e => e.Major).HasMaxLength(4);
+
+                entity.HasOne(d => d.MajorNavigation)
+                    .WithMany(p => p.Students)
+                    .HasForeignKey(d => d.Major)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("major_ref_department");
+            });
+
+            modelBuilder.Entity<Submission>(entity =>
+            {
+                entity.HasKey(e => new { e.StudentId, e.AssignmentId })
+                    .HasName("PRIMARY")
+                    .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+
+                entity.ToTable("Submission");
+
+                entity.HasIndex(e => e.AssignmentId, "aID_ref_assignments");
+
+                entity.Property(e => e.StudentId)
+                    .HasMaxLength(8)
+                    .HasColumnName("studentID");
+
+                entity.Property(e => e.AssignmentId)
+                    .HasColumnType("int(10) unsigned")
+                    .HasColumnName("assignmentID");
+
+                entity.Property(e => e.Contents).HasMaxLength(8192);
+
+                entity.Property(e => e.Score).HasColumnType("int(10) unsigned");
+
+                entity.Property(e => e.Time).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Assignment)
+                    .WithMany(p => p.Submissions)
+                    .HasForeignKey(d => d.AssignmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("aID_ref_assignments");
+
+                entity.HasOne(d => d.Student)
+                    .WithMany(p => p.Submissions)
+                    .HasForeignKey(d => d.StudentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("studentID_ref_students");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
